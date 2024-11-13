f0e38a24c1f18c684a8f7e98658e652a176e0f08
kenichi-akira <161388141+kenichi-akira@users.noreply.github.com>
2024-11-13 20:05:57 -0300

    Add files via upload

---
+++ src/components/Section/index.jsx
@@ -0,0 +1,47 @@
+import { useRef } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Container, Slider } from './styles';
+
+export function Section({title, children}) {
+  const slider = useRef(null);
+
+  function handleLeftClick(e) {
+    e.preventDefault();
+
+    slider.current.scrollLeft -= slider.current.offsetWidth;
+  }
+
+  function handleRightClick(e) {
+    e.preventDefault();
+
+    slider.current.scrollLeft += slider.current.offsetWidth;
+  }
+
+   return (
+    <Container>
+      <h2>{title}</h2>
+
+      <Slider>
+        <button 
+          className='btn-left'
+          onClick={handleLeftClick}
+        >
+          <FiChevronLeft size={50} />
+        </button>
+
+        <div ref={slider}>
+          {children}
+        </div>
+
+        <button 
+          className='btn-right'
+          onClick={handleRightClick}
+        >
+          <FiChevronRight size={50}/>
+        </button>
+      </Slider>
+
+    </Container>
+   )
+}
